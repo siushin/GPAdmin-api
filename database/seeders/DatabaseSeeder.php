fd184426eb26277611f0a0c01d559745fd10ac39
siushin <siushin@163.com>
2026-01-28 23:47:34 +0800

    Update DatabaseSeeder and composer.json to use Admin module seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Modules\Base\Database\Seeders\BaseDatabaseSeeder;
+use Modules\Admin\Database\Seeders\BaseDatabaseSeeder;
 use Modules\Base\Services\MenuImportService;
 
 class DatabaseSeeder extends Seeder
